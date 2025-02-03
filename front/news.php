b31994bd3401bf99f70730a3231fe2a0a5701454
Olivia Wu <oliviawu0301@gmail.com>
2025-02-03 23:05:06 +0800

    new追加動態圖片功能

--- front/news.php
+++ front/news.php
@@ -1,8 +1,17 @@
 <div class="di"
     style="height:540px; border:#8c632d 1px solid; width:53.5%; margin:2px 0px 0px 0px;  float:left; position:relative; left:30px;">
-    <marquee scrolldelay="120" direction="left" style="position:absolute; width:100%; height:40px;">
+    <marquee scrolldelay="120" direction="left" style="position:absolute; width:90%; height:40px;">
+        <?php
+        $ads = $Ad->all(['sh' => 1]);
+        foreach ($ads as $ad) {
+            echo $ad['text'];
+            echo str_repeat("&nbsp;", 10);
+        }
+        ?>
     </marquee>
-    <div style="height:32px; text-align:center; display:block; background-color:#0C3;">跑馬燈區</div>
+    <div style="height:32px; text-align:center; display:block;">
+    </div>
+
     <!--正中央-->
     <script>
         var lin = new Array();
@@ -21,7 +30,14 @@
                 now = 0;
         }
     </script>
-    <div style="width:99%; height:50%; position:relative;" class="cent">無任何資料</div>
+    <div style="width:99%; height:50%; position:relative;" class="cent">
+        <?php
+            $mvims = $Mvim->all(['sh' => 1]);
+            foreach ($mvims as $mvim) {
+                echo "<img style='height:65%' src='upload/{$mvim['img']}'>";
+            };
+        ?>
+    </div>
     <div style="width:95%; padding:2px; height:190px; margin-top:10px; padding:5px 10px 5px 10px;  position:relative;">
         <span class="t botli">最新消息區</span>
         <div style="width:100%; padding:2px; height:290px;">
