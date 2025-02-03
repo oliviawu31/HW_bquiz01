7348cdda0964f6ccf78e1d197253fa33cba0a322
Olivia Wu <oliviawu0301@gmail.com>
2025-02-03 23:14:14 +0800

    renew db

--- hw.sql
+++ hw.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- 主機： 127.0.0.1
--- 產生時間： 2025-02-03 09:12:23
--- 伺服器版本： 10.4.32-MariaDB
--- PHP 版本： 8.2.12
+-- 主機： localhost
+-- 產生時間： 2025 年 02 月 03 日 16:12
+-- 伺服器版本： 10.4.28-MariaDB
+-- PHP 版本： 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -28,7 +28,7 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `admin` (
-  `id` int(10) UNSIGNED NOT NULL,
+  `id` int(5) NOT NULL,
   `acc` text NOT NULL,
   `pw` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
@@ -38,8 +38,8 @@ CREATE TABLE `admin` (
 --
 
 INSERT INTO `admin` (`id`, `acc`, `pw`) VALUES
-(1, 'admin', '1234'),
-(2, 'root123', '123456');
+(6, '123', '123'),
+(7, '345', '345');
 
 -- --------------------------------------------------------
 
@@ -48,9 +48,9 @@ INSERT INTO `admin` (`id`, `acc`, `pw`) VALUES
 --
 
 CREATE TABLE `ads` (
-  `id` int(10) UNSIGNED NOT NULL,
+  `id` int(5) NOT NULL,
   `text` text NOT NULL,
-  `sh` int(1) UNSIGNED NOT NULL DEFAULT 1
+  `sh` int(1) NOT NULL DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
@@ -58,9 +58,8 @@ CREATE TABLE `ads` (
 --
 
 INSERT INTO `ads` (`id`, `text`, `sh`) VALUES
-(1, 'NEW IN ! MARSHVILLE HOLIDAY', 1),
-(2, 'HAPPY LUNAR NEW YEAR!', 1),
-(3, 'Welcome to DINOTAENG', 1);
+(4, 'hello!!', 1),
+(5, 'hi!!!!', 1);
 
 -- --------------------------------------------------------
 
@@ -69,7 +68,7 @@ INSERT INTO `ads` (`id`, `text`, `sh`) VALUES
 --
 
 CREATE TABLE `bottom` (
-  `id` int(5) NOT NULL,
+  `id` int(10) UNSIGNED NOT NULL,
   `bottom` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
@@ -78,7 +77,7 @@ CREATE TABLE `bottom` (
 --
 
 INSERT INTO `bottom` (`id`, `bottom`) VALUES
-(1, 'copyright');
+(1, 'Olivia版權所有｜ 2025');
 
 -- --------------------------------------------------------
 
@@ -97,10 +96,10 @@ CREATE TABLE `images` (
 --
 
 INSERT INTO `images` (`id`, `img`, `sh`) VALUES
-(1, 'Carousel-1.jpg', 1),
-(2, 'Carousel-2.jpg', 1),
-(3, 'Carousel-3.jpg', 1),
-(4, 'Carousel-4.jpg', 1);
+(5, 'Carousel-1.jpg', 1),
+(6, 'Carousel-2.jpg', 1),
+(7, 'Carousel-3.jpg', 1),
+(8, 'Carousel-4.jpg', 1);
 
 -- --------------------------------------------------------
 
@@ -133,9 +132,9 @@ CREATE TABLE `mvims` (
 --
 
 INSERT INTO `mvims` (`id`, `img`, `sh`) VALUES
-(5, '1-1.gif', 1),
-(6, '1-2.gif', 1),
-(7, '1-3.gif', 1);
+(6, '1-1.gif', 1),
+(7, '1-2.gif', 1),
+(8, '1-3.gif', 0);
 
 -- --------------------------------------------------------
 
@@ -144,9 +143,9 @@ INSERT INTO `mvims` (`id`, `img`, `sh`) VALUES
 --
 
 CREATE TABLE `news` (
-  `id` int(10) UNSIGNED NOT NULL,
+  `id` int(5) NOT NULL,
   `text` text NOT NULL,
-  `sh` int(1) UNSIGNED NOT NULL DEFAULT 1
+  `sh` int(1) NOT NULL DEFAULT 1
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
@@ -154,10 +153,17 @@ CREATE TABLE `news` (
 --
 
 INSERT INTO `news` (`id`, `text`, `sh`) VALUES
-(1, '123', 1),
-(2, '456', 1),
-(3, '789', 1),
-(4, '789', 1);
+(1, '教師研習「世界公民生命園丁國內研習會」\r\n1.主辦單位：世界展望會\r\n2.研習日期：101年11月14日（三）～15日（四）\r\n3.詳情請參考：\r\nhttp://gc.worldvision.org.tw/seed.html。\r\n請線上報名。', 1),
+(2, '公告綜合高中一年級英數補救教學時間\r\n上課日期:10/27.11/3.11/10.11/24共計四次\r\n上課時間:早上8:00~11:50半天\r\n費用:全程免費\r\n參加同學:綜合科一年級第一次段考成績需加強者\r\n已將名單送交各班及導師\r\n參加同學請帶紙筆.課本.第一次段考考卷\r\n並將家長通知單給家長\r\n若有任何疑問\r\n請洽綜合高中學程主任', 1),
+(3, '102年全國大專校院運動會\r\n「主題標語及吉祥物命名」\r\n網路票選活動\r\n一、活動期間：自10月25日起至11月4日止。\r\n二、相關訊息請上宜蘭大學首頁連結「102全大運在宜大」\r\n活動網址：http://102niag.niu.edu.tw/', 1),
+(4, '台灣亞洲藝術文化教育交流學會第一屆年會國際研討會\r\n活動日期：101年3月3～4日(六、日)\r\n活動主題：創造力、文化、全人教育\r\n有意參加者請至http://www.caaetaiwan.org下載報名表', 1),
+(5, '11月23日(星期五)將於彰化縣田尾鄉菁芳園休閒農場\r\n舉辦「高中職生涯輔導知能研習」\r\n中區學校每校至多2名\r\n以普通科、專業類科教師優先報名參加\r\n生涯規劃教師次之，參加人員公差假\r\n並核實派代課\r\n當天還有專車接送(8:35前在員林火車站集合)\r\n如此好康的機會，怎能錯過？！\r\n熱烈邀請師長們向輔導室(分機234)報名\r\n名額有限，動作要快！！\r\n報名截止日期：本周四 10月25日17:00前！', 1),
+(7, '實際上應該是表示一個單引號。', 1),
+(8, '實際上應該是表示一個單引號。', 1),
+(9, '實際上應該是表示一個單引號。', 1),
+(10, 'ceil() ceil($數值); 無條件進位到整數位；', 1),
+(11, 'ceil() 這個指令跟上一條floor() ', 1),
+(12, '[PHP] floor() ceil() round() 無條件捨去、進位、四捨五入', 1);
 
 -- --------------------------------------------------------
 
@@ -177,10 +183,10 @@ CREATE TABLE `titles` (
 --
 
 INSERT INTO `titles` (`id`, `img`, `text`, `sh`) VALUES
-(1, 'banner-01.png', 'orange', 0),
-(2, 'banner-02.png', 'pink', 1),
-(3, 'banner-03.png', 'blue', 0),
-(4, 'banner-04.png', 'green', 0);
+(23, '01-orange.png', 'orange', 0),
+(24, '02-pink.png', 'pink', 1),
+(25, '03-blue.png', 'blue', 0),
+(26, '04-green.png', 'green', 0);
 
 -- --------------------------------------------------------
 
@@ -189,9 +195,9 @@ INSERT INTO `titles` (`id`, `img`, `text`, `sh`) VALUES
 --
 
 CREATE TABLE `total` (
-  `id` int(10) UNSIGNED NOT NULL,
+  `id` int(11) NOT NULL,
   `date` date NOT NULL,
-  `total` int(10) UNSIGNED NOT NULL
+  `total` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
@@ -199,39 +205,8 @@ CREATE TABLE `total` (
 --
 
 INSERT INTO `total` (`id`, `date`, `total`) VALUES
-(2, '0000-00-00', 1),
-(3, '0000-00-00', 1),
-(4, '0000-00-00', 1),
-(5, '0000-00-00', 1),
-(6, '0000-00-00', 1),
-(7, '0000-00-00', 1),
-(8, '0000-00-00', 1),
-(9, '0000-00-00', 1),
-(10, '0000-00-00', 1),
-(11, '0000-00-00', 1),
-(12, '0000-00-00', 1),
-(13, '0000-00-00', 1),
-(14, '0000-00-00', 1),
-(15, '0000-00-00', 1),
-(16, '0000-00-00', 1),
-(17, '0000-00-00', 1),
-(18, '0000-00-00', 1),
-(19, '0000-00-00', 1),
-(20, '0000-00-00', 1),
-(21, '0000-00-00', 1),
-(22, '0000-00-00', 1),
-(23, '0000-00-00', 1),
-(24, '0000-00-00', 1),
-(25, '0000-00-00', 1),
-(26, '0000-00-00', 1),
-(27, '0000-00-00', 1),
-(28, '0000-00-00', 1),
-(29, '0000-00-00', 1),
-(30, '0000-00-00', 1),
-(31, '0000-00-00', 1),
-(32, '0000-00-00', 1),
-(33, '0000-00-00', 1),
-(34, '0000-00-00', 1);
+(1, '2025-02-03', 178),
+(1, '2025-02-03', 178);
 
 --
 -- 已傾印資料表的索引
@@ -286,12 +261,6 @@ ALTER TABLE `titles`
   ADD PRIMARY KEY (`id`);
 
 --
--- 資料表索引 `total`
---
-ALTER TABLE `total`
-  ADD PRIMARY KEY (`id`);
-
---
 -- 在傾印的資料表使用自動遞增(AUTO_INCREMENT)
 --
 
@@ -299,55 +268,49 @@ ALTER TABLE `total`
 -- 使用資料表自動遞增(AUTO_INCREMENT) `admin`
 --
 ALTER TABLE `admin`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- 使用資料表自動遞增(AUTO_INCREMENT) `ads`
 --
 ALTER TABLE `ads`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- 使用資料表自動遞增(AUTO_INCREMENT) `bottom`
 --
 ALTER TABLE `bottom`
-  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- 使用資料表自動遞增(AUTO_INCREMENT) `images`
 --
 ALTER TABLE `images`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- 使用資料表自動遞增(AUTO_INCREMENT) `menus`
 --
 ALTER TABLE `menus`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- 使用資料表自動遞增(AUTO_INCREMENT) `mvims`
 --
 ALTER TABLE `mvims`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- 使用資料表自動遞增(AUTO_INCREMENT) `news`
 --
 ALTER TABLE `news`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- 使用資料表自動遞增(AUTO_INCREMENT) `titles`
 --
 ALTER TABLE `titles`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
---
--- 使用資料表自動遞增(AUTO_INCREMENT) `total`
---
-ALTER TABLE `total`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
